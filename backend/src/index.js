9a9e950b8e76e371850a881dc9f30c4a4f496835
Sk Rizwan <rizwansk@google.com>
2026-02-18 23:15:00 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,28 @@
+import express from "express"
+import dotenv from "dotenv"
+import {connectDB} from "./lib/db.js"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
+
+dotenv.config()
+const app = express();
+
+const PORT = process.env.PORT
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
+
+app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
+
+app.listen(PORT, ()=>{
+    console.log("Server is running on port:" + PORT);
+    connectDB()
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,49 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingPage from "./pages/SettingPage";
+import ProfilePage from "./pages/ProfilePage";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+import {Loader} from "lucide-react"
+import {Toaster} from "react-hot-toast"
+
+const App = () => {
+
+const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
+
+useEffect(()=>{
+  checkAuth()
+},[checkAuth]);
+
+console.log({authUser});
+
+if(isCheckingAuth && !authUser) return (
+
+  <div className="flex items-center justify-center h-screen">
+    <Loader className="size-10 animate-spin"/>
+  </div>
+)
+
+
+  return (
+    <div>
+      
+      <NavBar/>
+
+      <Routes>
+        <Route path="/" element={authUser ? <HomePage/> : <Navigate to="/login"/>} />
+        <Route path="/signup" element={!authUser ? <SignUpPage/> : <Navigate to="/"/>} />
+        <Route path="/login" element={!authUser ? <LoginPage/> : <Navigate to="/"/>} />
+        <Route path="/setting" element={<SettingPage/>} />
+        <Route path="/profile" element={authUser ? <ProfilePage/> : <Navigate to="/login"/>} />
+      </Routes>
+
+      <Toaster/>
+    </div>
+  )
+}
+
+export default App;
