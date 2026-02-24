b90e2384765acb0a64c42e2b13ffa7589b60a50a
Sk Rizwan <rizwansk@google.com>
2026-02-24 23:23:47 +0530

    package fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,29 +1,42 @@
-import express from "express"
-import dotenv from "dotenv"
-import {connectDB} from "./lib/db.js"
-import cookieParser from "cookie-parser"
-import cors from "cors"
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
-import { app, server } from "./lib/socket.js"
+import path from "path";
 
-dotenv.config()
+import { connectDB } from "./lib/db.js";
 
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
-const PORT = process.env.PORT
+dotenv.config();
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
     origin: "http://localhost:5173",
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
-app.use("/api/auth", authRoutes)
-app.use("/api/messages", messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-server.listen(PORT, ()=>{
-    console.log("Server is running on port:" + PORT);
-    connectDB()
-})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
